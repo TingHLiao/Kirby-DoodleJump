e51f991b29e3261e6175d8f0ff4d12d612060757
TingHsuan <sharon5533078@gmail.com>
2020-06-25 14:31:53 +0800

    UI update

---
+++ assets/Script/start.ts
@@ -0,0 +1,33 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Start extends cc.Component {
+
+    @property(cc.Node)
+    label: cc.Node = null;
+
+    fadeAction: cc.Action = null;
+
+    onLoad() {
+        this.fadeAction = this.setAction();
+    }
+
+    start () {
+        this.schedule(function (){
+            this.label.runAction(this.fadeAction);
+        },2.7);
+
+        this.node.on('mousedown', function(event){
+            if(event.getButton() == 0){
+                cc.director.loadScene("Menu");
+            }
+        }, this);
+    }
+
+    setAction() {
+        var fadeIn = cc.fadeIn(0.5);
+        var delay = cc.delayTime(0.5);
+        var fadeOut = cc.fadeOut(1.5);
+        return cc.sequence(fadeIn, delay, fadeOut);
+    }
+}
