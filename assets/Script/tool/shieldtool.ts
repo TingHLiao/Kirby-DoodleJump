897e285fec801f0e023f473d8ed8c8a8ff746089
TingHsuan <sharon5533078@gmail.com>
2020-06-25 19:35:19 +0800

    shield update

--- assets/Script/tool/shieldtool.ts
+++ assets/Script/tool/shieldtool.ts
@@ -3,6 +3,9 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class ShieldTool extends cc.Component {
 
+    @property({ type: cc.AudioClip })
+    soundEffect: cc.AudioClip = null;
+    
     @property(cc.Prefab)
     shield: cc.Prefab = null;
 
@@ -17,6 +20,7 @@ export default class ShieldTool extends cc.Component {
     onBeginContact(contact, self, other) {
         if(other.tag === 0){
             contact.disabled = true;
+            cc.audioEngine.playEffect(this.soundEffect, false);
             var newnode = cc.instantiate(this.shield);
             other.node.addChild(newnode);
             self.node.destroy();
