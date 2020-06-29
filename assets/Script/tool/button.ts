f1c1dc7fa55837ab0641d6fe28c7d66952bb6950
王劭元 <albert@wangshaoyuandeMacBook-Pro.local>
2020-06-29 13:35:31 +0800

    add play scene sound effect

---
+++ assets/Script/tool/button.ts
@@ -0,0 +1,32 @@
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class button extends cc.Component {
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    @property({type: cc.AudioClip})
+    clickEffect: cc.AudioClip = null;
+
+    start () {
+
+    }
+
+    onclick(){
+        cc.audioEngine.playEffect(this.clickEffect, false);
+    }
+
+    // update (dt) {}
+}
